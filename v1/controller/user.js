0299ff22df32d0f45245ac2012e6e553fcb1cc4e
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2021-11-13 18:51:25 +0530

    Added signin route controller

--- v1/controller/user.js
+++ v1/controller/user.js
@@ -3,6 +3,12 @@
 const universal = require("../../constants/index.js");
 const MODELS = require("../../models/index");
 
+function getRandom(length) {
+
+  return Math.floor(Math.pow(10, length-1) + Math.random() * 9 * Math.pow(10, length-1));
+
+}
+
 module.exports = {
   signIn: signIn,
   signUp: signUp
@@ -48,9 +54,35 @@ function signUp(req, res, next) {
 function signIn(req, res, next) {
   async function signIn() {
     try {
-      return res.send("Login is Working");
+    const { email , password } = req.body
+
+    const userEmail = email.toLowerCase().trim()
+
+    const user = await MODELS.user.findOne({ userEmail })
+   
+    if(user){
+        return res.status(universal.CODES.BAD_REQUEST).send(universal.MESSAGES.USER_ALREADY_EXIST)
+    }
+    
+    if(user && user.password === password) {
+        user.isActive = true;
+        user.sessionId = getRandom(10);
+        const updatedUser = await user.save()
+        console.log(updatedUser)
+
+        res.json({
+            name : updatedUser.name,
+            email: updatedUser.email,
+            sessionId: updatedUser.sessionId,
+            password: updatedUser.password,
+            transactionId: updatedUser.transactionId
+        })
+    }
+    else{
+      return res.status(universal.CODES.NOT_FOUND).send(universal.MESSAGES.USER_DOEST_NOT_EXIST)
+    }
     } catch(err) {
-      next(err);
+      return res.status(universal.CODES.INTERNAL_SERVER_ERROR).send(universal.MESSAGES.INTERNAL_ERROR)    
     }
   }
   signIn().then(function () { });
